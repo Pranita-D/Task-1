714a3cbc8a2a6e18e8c7ccae91284698d5ae585c
Pranita-D <126228956+Pranita-D@users.noreply.github.com>
2023-02-24 16:01:49 +0530

    Update Pythoncode.py

--- Pythoncode.py
+++ Pythoncode.py
@@ -3,7 +3,7 @@ import os
 import time
 import subprocess
 
-# Define the directory to search for files
+# Directory Path
 directory = '/home/ubuntu/myproject/'
 
 # 30 Min age of files to be deleted 
@@ -15,10 +15,9 @@ current_time = time.time()
 # All files in the directory
 for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
-    # Check if the file is older than the maximum age
+    # Check All file of the  the maximum age
     if os.path.isfile(file_path) and (current_time - os.path.getmtime(file_path)) > max_age:
         # Delete the file using curl command
-        subprocess.call(['curl', '-X', 'DELETE', 'https://localhost:8080/files/' + filename]))
-        # File Path Delete
+        subprocess.call(['curl', '-X', 'DELETE', 'https://localhost:8080/files/' + filename])
         os.remove(file_path)
         
